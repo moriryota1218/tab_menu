207c281d4091d0adf900389bb99fda45647833e6
森亮太 <moriryouta@moriryoutanoMacBook-ea.local>
2019-05-19 22:00:22 +0900

    finish

--- js/main.js
+++ js/main.js
@@ -2,15 +2,27 @@
 {
   // メニュー項目を定数で取得
   const menuItems = document.querySelectorAll('.menu li a');
+  const contents = document.querySelectorAll('.content');
 
   // forEach()を使って各要素に対してイベントを設定
-  menuItems.forEach(item => {
-  // クリックイベントの設定
-  item.addEventListener('click', e => { //Eventオブジェクトを受け取る
-    e.preventDefault();
+  menuItems.forEach(clickedItem => {
+    // クリックイベントの設定
+    clickedItem.addEventListener('click', e => { //Eventオブジェクトを受け取る
+      e.preventDefault();
 
-    item.classList.add('active');
-  });
+      // activeクラスを外す処理
+      menuItems.forEach(item => {
+        item.classList.remove('active');
+      });
+
+      clickedItem.classList.add('active');
+
+      // conentからactiveクラスを外す処理
+      contents.forEach(content => {
+        content.classList.remove('active');
+      });
+      document.getElementById(clickedItem.dataset.id).classList.add('active');
+    });
 
   });
 }
